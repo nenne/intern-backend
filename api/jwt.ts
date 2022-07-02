29ac3a46d599ed19845466acbe0ba1ea8275c0a1
一般入試 <umi_koneko@outlook.jp>
2022-07-02 21:16:20 +0900

    jwtちょっとわかる...?になった

--- api/jwt.ts
+++ api/jwt.ts
@@ -1,2 +1,23 @@
-//wakanne-------
-//nanjakorya
+import express from "express";
+import cookieParser from "cookie-parser";
+
+import passport from "./sec";
+import userRouter from "./repositories/user";
+
+// 1 expressの設定
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// 2 passportを初期化
+app.use(passport.initialize());
+// 3 routerを追加
+app.use("/user", passport.authenticate("jwt", { session: false }), userRouter);
+
+app.listen(3000, () => {
+  console.log("listen to " + 3000);
+});
+
+//jwtメモ　https://zenn.dev/msksgm/articles/20211113-typescript-jwt-passport-server
--- api/repositories/user.ts
+++ api/repositories/user.ts
@@ -1,6 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
 
 const prisma = new PrismaClient();
+
+const router = Router();
+
+router.get("/1", (req, res, next) => {
+  res.json("number of user is 1");
+});
+
+router.get("/2", (req, res, next) => {
+  res.json("number of user is 2");
+});
+
+export default router;
 
 //createuser signupに使う
 //finduser loginに使う
